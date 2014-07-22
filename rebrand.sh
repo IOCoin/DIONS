5f847f7bf11ff79474cc8f298825c705ab16d800
I/OCoin <IOCoinDev@gmail.com>
2014-07-22 15:40:59 -0400

    Update rebranding script.

--- rebrand.sh
+++ rebrand.sh
@@ -2,4 +2,6 @@
 
 find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i 's/iocoin/iocoin/g'
 find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i 's+I/OCoin+I/OCoin+g'
+find src/qt/ -type f -iname "*.ui" -or -iname "*.ts" -or -name "bitcoinunits.cpp" -print0 | xargs -0 sed -i 's/BC/IO/g'
+find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i 's/iphU6HbZCuDsx1nGWncuwxYdHcF74zLv8U/iphU6HbZCuDsx1nGWncuwxYdHcF74zLv8U/g'
 mv iocoin-qt.pro iocoin-qt.pro
