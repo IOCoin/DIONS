d6bfa70fece01cc0d7f48642b42578447318032d
I/OCoin <IOCoinDev@gmail.com>
2014-07-22 16:02:00 -0400

    Update DNS seed to real one.

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"official.iocoin.co", "seed.iocoin.co"},
+    {"official.seed", "useast.iocoin.io"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
