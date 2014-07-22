5b3bee46846fde21d99dae647a1d98feb1ed5f44
I/OCoin <IOCoinDev@gmail.com>
2014-07-22 16:00:18 -0400

    Update rebranding script.

--- rebrand.sh
+++ rebrand.sh
@@ -2,6 +2,9 @@
 
 find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i 's/iocoin/iocoin/g'
 find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i 's+I/OCoin+I/OCoin+g'
-find src/qt/ -type f -iname "*.ui" -or -iname "*.ts" -or -name "bitcoinunits.cpp" -print0 | xargs -0 sed -i 's/BC/IO/g'
+find src/qt/ -type f -iname "*.ui" -or -iname "*.ts" -or -name "bitcoinunits.cpp" -print0 | xargs -0 sed -i 's|BC|I/O|g'
 find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i 's/iphU6HbZCuDsx1nGWncuwxYdHcF74zLv8U/iphU6HbZCuDsx1nGWncuwxYdHcF74zLv8U/g'
-mv iocoin-qt.pro iocoin-qt.pro
+find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i 's|0.00 BC|0.00 I/O|g'
+find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i 's|0 BC|0 I/O|g'
+find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i 's|123.456 BC|123.456 I/O|g'
+mv blackcoin-qt.pro iocoin-qt.pro
