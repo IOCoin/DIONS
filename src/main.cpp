bad3458a1bd0e415c9100958e07512b735879a3e
I/OCoin <IOCoinDev@gmail.com>
2014-07-22 15:48:58 -0400

    Blocks mature after 100 confirms.

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ unsigned int nStakeMinAge = 8 * 60 * 60; // 8 hours
 unsigned int nStakeMaxAge = -1; // unlimited
 unsigned int nModifierInterval = 10 * 60; // time to elapse before new modifier is computed
 
-int nCoinbaseMaturity = 500;
+int nCoinbaseMaturity = 100;
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
 
