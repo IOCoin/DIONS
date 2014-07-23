e053fd6c8ced8d1b42327d425ef5b173b6b3b97e
I/OCoin <IOCoinDev@gmail.com>
2014-07-23 10:08:57 -0400

    Update graphics.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -83,7 +83,7 @@ static void InitMessage(const std::string &message)
 {
     if(splashref)
     {
-        splashref->showMessage(QString::fromStdString(message), Qt::AlignBottom|Qt::AlignHCenter, QColor(232,186,63));
+        splashref->showMessage(QString::fromStdString(message), Qt::AlignVCenter|Qt::AlignHCenter, QColor(33,120,185));
         QApplication::instance()->processEvents();
     }
 }
