92c80eb64ff409ca47058438acc513e5c9e1adbc
I/OCoin <IOCoinDev@gmail.com>
2014-07-28 21:58:34 -0400

    Switch DNS seed node.

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"useast.iocoin.io", "useast.iocoin.io"},
+    {"useast.iocoin.io", "seed.iocoin.io"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
