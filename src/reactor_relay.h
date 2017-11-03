3068ec7c8ec68bfa79eff1916d3198f7e238426f
iocrypto <dhatton@protonmail.com>
2017-11-03 18:34:16 +0100

    util, relay

--- src/reactor_relay.h
+++ src/reactor_relay.h
@@ -3,6 +3,10 @@
 
 #include "bbuffer.h"
 
+#define s_put(q,x) ((q)[(x)/64] |= (((uint64_t) 1) << (uint64_t) ((x)%64)))
+#define s_extract(q,x) (((q)[(x)/64] >> (uint64_t) ((x)%64)) & 1)
+#define xb__(q,x) ((q)[(x)/64] &= ((~((uint64_t) 0)) - (((uint64_t) 1) << (uint64_t) ((x)%64))))
+
 class RLWE__CTRL__ 
 {
   template<int N>
@@ -15,6 +19,7 @@ class RLWE__CTRL__
 class Acceptor
 {
   virtual void rwle__base() = 0;
+  virtual void rwle__trans__() = 0;
   virtual void* operator()() = 0;
 };
 
