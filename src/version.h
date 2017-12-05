522cfb6a776b980cc01679fa7c56fcbcf398c90f
quarkonium <dhatton@gmx.ch>
2017-12-05 15:00:12 +0100

    v, x base

--- src/version.h
+++ src/version.h
@@ -30,13 +30,14 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60017;
+static const int PROTOCOL_VERSION = 60018;
+static const int X1_PROTOCOL_VERSION = 60017;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = PROTOCOL_VERSION;
+static const int MIN_PEER_PROTO_VERSION = X1_PROTOCOL_VERSION;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
