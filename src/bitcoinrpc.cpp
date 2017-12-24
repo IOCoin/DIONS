95fcacee74dcbb57ef147ebb316983ed982646e1
quarkonium <dhatton@gmx.ch>
2017-12-24 03:25:46 +0100

    log, mem

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -300,6 +300,7 @@ static const CRPCCommand vRPCCommands[] =
     { "dumpwallet",             &dumpwallet,             true,   false },
     { "importwallet",           &importwallet,           false,  false },
     { "crawgen",                &crawgen,   false,  false },
+    { "rmtx",                   &rmtx,   false,  false },
     { "importprivkey",          &importprivkey,          false,  false },
     { "listunspent",            &listunspent,            false,  false },
     { "getrawtransaction",      &getrawtransaction,      false,  false },
@@ -1292,6 +1293,7 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "listunspent"            && n > 1) ConvertTo<int64_t>(params[1]);
     if (strMethod == "listunspent"            && n > 2) ConvertTo<Array>(params[2]);
     if (strMethod == "crawgen") { ConvertTo<double>(params[0]); ConvertTo<Object>(params[1]); }
+    if (strMethod == "rmtx") { ConvertTo<double>(params[0]); ConvertTo<Object>(params[1]); }
     if (strMethod == "getrawtransaction"      && n > 1) ConvertTo<int64_t>(params[1]);
     if (strMethod == "createrawtransaction"   && n > 0) ConvertTo<Array>(params[0]);
     if (strMethod == "createrawtransaction"   && n > 1) ConvertTo<Object>(params[1]);
--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -2147,3 +2147,23 @@ Value makekeypair(const Array& params, bool fHelp)
     result.push_back(Pair("PublicKey", HexStr(key.GetPubKey().Raw())));
     return result;
 }
+
+Value rmtx(const Array& params, bool fHelp)
+{
+    if (fHelp || params.size() != 1)
+        throw runtime_error(
+            "rmtx txid\n"
+            "Removes transaction id from memory pool.");
+
+    uint256 hash;
+    hash.SetHex(params[0].get_str());
+
+    CTransaction tx;
+    uint256 hashBlock = 0;
+    if (!GetTransaction(hash, tx, hashBlock))
+        throw JSONRPCError(-5, "No information available about transaction in mempool");
+
+    mempool.remove(tx);
+    return tx.GetHash().GetHex();
+}
+
