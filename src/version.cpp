2bebed7869c8dbc0049a3811f16f0c6bc5c83160
quarkonium <dhatton@gmx.ch>
2018-06-16 17:32:31 +0200

    ragnorok

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Satoshi");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "66ce4f3"
+#    define GIT_COMMIT_ID "c871104"
 #    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
