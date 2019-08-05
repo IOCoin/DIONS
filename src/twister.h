2a5e00deb8857d545dc6cf0c170dd91b5dae6431
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2019-08-05 23:58:33 +0000

    util , bits engine type, mix

--- src/twister.h
+++ src/twister.h
@@ -5,12 +5,16 @@
 #include "boost/math/constants/constants.hpp"
 #include "boost/multiprecision/cpp_int.hpp"
 #include "boost/multiprecision/cpp_dec_float.hpp"
+#include "boost/random/independent_bits.hpp"
 
 #include <vector>
 
 using namespace::std;
 
-using namespace::boost::multiprecision;
+using namespace boost::multiprecision;
+using namespace boost::random;
+
+typedef independent_bits_engine<mt19937, 256, cpp_int> GEN__;
 
 struct displ
 {
