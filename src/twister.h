723da9a6f31a1de2c20817e85081f4a1ca45f84e
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2019-08-15 21:59:11 +0200

    test sequence, util reference freq dist , entropy

--- src/twister.h
+++ src/twister.h
@@ -69,4 +69,35 @@ struct TransitionElement
   vector<FI1__> reference_;
 };
 
+vector<double> f_dist(vector<unsigned char>& in)
+{
+  vector<double> fdist;
+  fdist.resize(256);
+  for(unsigned i = 0; i<in.size(); i++)
+  {
+    fdist[in[i]]++;
+  }
+
+  for(unsigned i=0; i<fdist.size(); i++)
+  {
+    fdist[i] = (fdist[i] / in.size()) * 100;
+  }
+
+  return fdist;
+}
+
+double s_entropy(vector<double> v) 
+{
+  double entropy = 0;
+  double p;
+
+  for(unsigned i = 0; i < v.size(); i++) 
+  {
+    p = v[i] / 100;
+    if (p == 0) continue;
+    entropy += p * std::log(p) / std::log(2);
+  }
+  return -entropy;
+}
+
 #endif
