f47183440acf87a008b4f1cc0920e5e8c4ecd465
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2019-10-06 23:51:01 +0000

    proto api test trans hom

--- src/twister.h
+++ src/twister.h
@@ -109,5 +109,6 @@ void trans(vector<unsigned char>& data, unsigned char (*f)(unsigned char));
 double s_entropy(vector<double>&) ;
 void switchIO(unsigned char (*p)(unsigned char, unsigned char), unsigned char);
 void transHom(vector<unsigned char>& , unsigned char (*f)(unsigned char), unsigned char (*g)(unsigned char));
+void hPerm(int s, int n, void (*p)(int), void (*inv)(int, int), void (*center)(int));
 
 #endif
