6a13c15a2785e0ab94f503b95db5c41bb6db69cb
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2019-10-08 23:40:22 +0000

    test, api updateutil

--- src/twister.h
+++ src/twister.h
@@ -111,5 +111,6 @@ void switchIO(unsigned char (*p)(unsigned char, unsigned char), unsigned char);
 void transHom(vector<unsigned char>& , unsigned char (*f)(unsigned char), unsigned char (*g)(unsigned char));
 void hPerm(int s, int n, void (*p)(int), void (*inv)(int, int), void (*center)(int));
 double sw(double weight, int i, int j, int (*inv)(int, int));
+void rms(const string& , string& );
 
 #endif
