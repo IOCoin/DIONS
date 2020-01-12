e3e7f83df7ce5619e0611ddeb7afb3706016b0af
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2020-01-12 19:16:20 +0000

    fmwk base-line nh

--- src/twister.cpp
+++ src/twister.cpp
@@ -223,6 +223,7 @@ std::tuple<int, int, int> extended_gcd(int __alpha, int __beta, int (*col)(int x
   return make_tuple(__com, y-(__beta/__alpha)*x, x);
 }
 //frmwk wrapper pending nh release, util base
+//
 std::tuple<int, int, int> extended_gcd(int __alpha, int __beta)
 {
   if(__alpha == 0) return make_tuple(__beta,0,1);
