8cdc929d30d3a3e335b9c3643ca3f084a92a0de3
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2020-03-04 19:04:46 +0000

    seq field, update with stream ref  plus init

--- src/reference.h
+++ src/reference.h
@@ -6,13 +6,21 @@
 
 using namespace std;
 
+struct 
+{
+  unsigned int is_init : 1;
+  unsigned int is_lock : 1;
+  unsigned int is_end_vtx : 1;
+  unsigned int is_unlock_sen : 1;
+} seq_p;
+
 class Reference
 {
   public:
     Reference() : INIT_REF("0")
     {
     };
-    Reference(vchType k) { INIT_REF="0"; this->m_ = stringFromVch(k); }
+    Reference(vchType k) { seq_p.is_init = 1; INIT_REF="0"; this->m_ = stringFromVch(k); }
     bool operator()() { return this->m_ == INIT_REF; }
 
   private:
