e620e3bf8648f8b0ac574bf6cb0aea1eabd65be1
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2020-07-23 14:34:44 +0000

    iface, data trans base hook

--- src/ext_tor.h
+++ src/ext_tor.h
@@ -7,6 +7,8 @@ class ExtTor
     virtual void gr() == 0; 
     virtual void genbase() == 0; 
     virtual void field_embed() == 0; 
+    virtual void translate(int,int,int) == 0; 
+    virtual void shift(int) == 0; 
     ~virtual ExtTor();
   private:
 };
