26dbda28bd87f04634fff1aa63b7c32ba70b48ae
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2020-07-27 12:57:50 +0000

    if, dat stream merge

--- src/ext_tor.h
+++ src/ext_tor.h
@@ -14,6 +14,7 @@ class ExtTor
     virtual void aror(int) == 0; 
     virtual void arol(int) == 0; 
     virtual void streamx(vector<unsigned char>&) == 0; 
+    virtual void merge(vector<unsigned char>&) == 0; 
     ~virtual ExtTor();
   private:
 };
