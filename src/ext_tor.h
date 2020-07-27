b3004a5bf760ef33f56cceeb3c5bc2a993f9ca32
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2020-07-27 12:55:41 +0000

    iface, stream xor, arith shift

--- src/ext_tor.h
+++ src/ext_tor.h
@@ -1,6 +1,8 @@
 #ifndef EXT_TOR
 #define EXT_TOR
 
+#include<vector.h>
+
 class ExtTor
 {
   public:
@@ -9,6 +11,9 @@ class ExtTor
     virtual void field_embed() == 0; 
     virtual void translate(int,int,int) == 0; 
     virtual void shift(int) == 0; 
+    virtual void aror(int) == 0; 
+    virtual void arol(int) == 0; 
+    virtual void streamx(vector<unsigned char>&) == 0; 
     ~virtual ExtTor();
   private:
 };
