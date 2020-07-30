de709b51749d2f6464091c2ff4fb20391313a680
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2020-07-30 13:24:57 +0000

    toroid, base

---
+++ src/tor.cpp
@@ -0,0 +1,20 @@
+#include"ext_tor.h"
+
+
+class Toroid
+{
+  public: 
+    Toroid() {};
+    virtual void gr() {}; 
+    virtual void genbase() {}; 
+    virtual void field_embed() {}; 
+    virtual void translate(int,int,int) {}; 
+    virtual void shift(int) {}; 
+    virtual void aror(int) {}; 
+    virtual void arol(int) {}; 
+    virtual void streamx(std::vector<unsigned char>&) {}; 
+    virtual void merge(std::vector<unsigned char>&) {}; 
+    virtual void prod(std::vector<unsigned char>&,std::vector<unsigned char>&) {}; 
+    virtual ~Toroid() { };
+  private:
+};
