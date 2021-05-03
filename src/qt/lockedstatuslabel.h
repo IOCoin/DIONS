b9662dbe7c3d7bd127ddde2ff5b5492d592055b4
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2021-05-03 22:15:02 +0000

    pre release ux rewrite plus bootstrap extract plus extractor qt util quazip - production test harness

---
+++ src/qt/lockedstatuslabel.h
@@ -0,0 +1,24 @@
+#ifndef LOCKEDSTATUSLABEL_H
+#define LOCKEDSTATUSLABEL_H
+
+#include "clickablelabel.h"
+#include<QLabel>
+#include<QWidget>
+#include<Qt>
+
+class LockedStatusLabel : public ClickableLabel 
+{
+  Q_OBJECT
+  public:
+    explicit LockedStatusLabel(QWidget* parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
+    ~LockedStatusLabel();
+
+  signals:
+    void unlock(bool);
+
+  protected:
+    void mousePressEvent(QMouseEvent* e);
+    
+};
+
+#endif
