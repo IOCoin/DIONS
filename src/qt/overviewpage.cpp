a768c0bb99a9125a6cdba60494c2aa6ec77cda60
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2021-05-04 15:36:45 +0000

    handle signal from locked status icon - prod test harness - review - quarkonium

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -255,10 +255,10 @@ void OverviewPage::updateDisplayUnit()
 {
     if(model && model->getOptionsModel())
     {
-        if(currentBalance != -1)
-	{
-            setBalance(currentBalance, model->getStake(), currentUnconfirmedBalance, currentImmatureBalance);
-	}
+  if(currentBalance != -1)
+  {
+    setBalance(currentBalance, model->getStake(), currentUnconfirmedBalance, currentImmatureBalance);
+  }
 
         // Update txdelegate->unit with the current unit
         txdelegate->unit = model->getOptionsModel()->getDisplayUnit();
