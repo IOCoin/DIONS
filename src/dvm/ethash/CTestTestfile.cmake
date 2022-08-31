05457aaafa5a2e76550119d3c2ca7ac935feee04
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2022-08-31 22:07:51 +0000

    release candidate - test net - v1 - verify - test build

--- src/dvm/ethash/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ioc/DIONS/src/dvm/ethash
-# Build directory: /home/ioc/DIONS/src/dvm/ethash
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("test")
