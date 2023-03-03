1eb9958ca1c32ed5e73841bea7f79058ed251993
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2023-03-03 16:59:14 +0000

    bump protocol version

--- src/version.cpp
+++ src/version.cpp
@@ -39,7 +39,7 @@ const std::string CLIENT_NAME("Satoshi");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "d2468586"
+#    define GIT_COMMIT_ID "ab1e1fd8"
 #    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
