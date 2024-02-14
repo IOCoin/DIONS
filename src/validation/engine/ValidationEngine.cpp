f14166ac3abdc051ab8e2b6990ae551f35a76a49
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2024-02-14 19:37:07 +0000

    change pipeline configurabe for net - validator pipeline

--- src/validation/engine/ValidationEngine.cpp
+++ src/validation/engine/ValidationEngine.cpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include<vector>
-
 #include "validation/base/Validator.h"
 
 class ValidationEngine 
@@ -10,5 +8,4 @@ class ValidationEngine
     ValidationEngine() { }
 
   private:
-    std::vector<Validator> validiators;
 };
